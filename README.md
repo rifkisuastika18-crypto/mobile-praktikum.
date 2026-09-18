bb8b518d569483555fd5f59f10ec5530eb1e1bf3
Gede Rifki Swastika <rifkisuastika18@gmail.com>
2026-09-18 14:32:44 +0800

    Simpanan proyek awal perkuliahan

---
+++ README.md
@@ -0,0 +1,7 @@
+# Proyek Pemrograman Mobile
+## Deskripsi buat mobile aplication pesanan ojek
+Aplikasi latihan untuk prsanan
+## Pengembang
+Nama panggilan / akun GitHub: rifkisuastika18@gmail.com
+## Status
+Proyek awal perkuliahan.
---
+++ app/src/main/java/com/example/aplikasi_pertamarifki/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.aplikasi_pertamarifki
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aplikasi_pertamarifki.ui.theme.Aplikasi_pertamaRifkiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Aplikasi_pertamaRifkiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Aplikasi_pertamaRifkiTheme {
+        Greeting("Android")
+    }
+}
